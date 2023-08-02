bfd73850b1ea3ac8b7dd121aa827437dc5197336
natanaelvich <taelima1997@gmail.com>
2023-08-02 19:52:35 -0300

    update mock user

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -6,16 +6,16 @@ export function Profile() {
   return (
     <div className="flex items-center gap-3">
       <img
-        src="https://github.com/diego3g.png"
+        src="https://github.com/natanaelvich.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
       <div className="flex flex-col">
         <span className="block text-sm font-semibold text-zinc-700">
-          Diego Fernandes
+          Natanael Lima
         </span>
         <span className="block text-sm text-zinc-500">
-          diego.schell.f@gmail.com
+          taelima1997@gmail.com
         </span>
       </div>
       <LogOut className="ml-auto h-5 w-5 text-zinc-500" />
